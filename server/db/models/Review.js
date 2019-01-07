9ca11c3e4881c4b3bb1b9e0180a1a4b9a625a790
MJ Zefeldt <mjzefeldt@gmail.com>
2019-01-07 15:59:51 -0600

    review model issue 04 for review

---
+++ server/db/models/Review.js
@@ -0,0 +1,13 @@
+const db = require('../db')
+const Sequelize = require('sequelize')
+
+const {TEXT} = Sequelize
+
+const Review = db.define(review, {
+  text: {
+    type: TEXT,
+    allowNull: false
+  }
+})
+
+module.exports = Review
