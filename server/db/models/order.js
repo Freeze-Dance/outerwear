8c2be855d64e7330343cec29fcb906941994f2c0
Yaodi Hu <yaodijr@gmail.com>
2019-01-07 16:01:52 -0600

    syntax adjustment

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 const db = require('./db');
 
-const Order = db.define('orders', {
+const Order = db.define('order', {
   userId: {type: Sequelize.INTEGER},
   items: {type: Sequelize.ARRAY(Sequelize.INTEGER)},
   time: {type: Sequelize.DATE, defaultValue: Sequelize.NOW}
