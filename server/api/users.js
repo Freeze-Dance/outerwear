febc825c6768af0b5fdd526d98a755a50d1b5e32
Rami Dihu <rami.dihu@gmail.com>
2019-01-08 10:53:33 -0600

    Users Route

--- server/api/users.js
+++ server/api/users.js
@@ -1,17 +1,56 @@
 const router = require('express').Router();
 const {User} = require('../db/models');
-module.exports = router;
 
 router.get('/', async (req, res, next) => {
   try {
-    const users = await User.findAll({
-      // explicitly select only the id and email fields - even though
-      // users' passwords are encrypted, it won't help if we just
-      // send everything to anyone who asks!
-      attributes: ['id', 'email']
-    });
-    res.json(users);
+    const users = await User.findAll()
+      res.json(users)
   } catch (err) {
     next(err);
   }
 });
+
+// get user by userId
+router.get('/:userId', async(req, res, next) => {
+  try{
+    const singleUser = await User.findById(req.params.userId)
+    res.json(singleUser)
+  }catch(err){
+    next(err)
+  }
+})
+
+// create new user
+router.post('/createUser', async(req, res, next) => {
+  try{
+    if(!req.params.admin){
+      res.sendStatus(403)
+    }
+    const user = await User.create(req.body)
+    res.send(user)
+  }catch(err){
+    next(err)
+  }
+})
+
+// update user
+router.put('/:userId', async(req, res, next) => {
+  try{
+    if(!req.user.admin){
+      res.sendStatus(403)
+    }
+
+    const user = await User.update(req.body, {
+      returning: true,
+      where: {
+        id: req.params.userId
+      }
+    })
+    if(!user) return res.sendStatus(404)
+    res.send(user);
+  }catch(err){
+    next(err)
+  }
+})
+
+module.exports = router;
