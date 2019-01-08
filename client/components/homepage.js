9ad7864e8ca2d128d4366b70f3ee47c3ad6928ff
Jay Jongik Thom <ljik95@hotmail.com>
2019-01-08 16:59:24 -0600

    added stuff to navbar w/ other minor changes

--- client/components/homepage.js
+++ client/components/homepage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import store from '../store'
+// import AllProducts from './AllProducts'
 
 /**
  * COMPONENT
