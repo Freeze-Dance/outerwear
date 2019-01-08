1c8551e78e262d5e5d77be9c92dbf8adafd90c3b
Yaodi Hu <yaodijr@gmail.com>
2019-01-08 09:12:48 -0600

    changed navbar

--- client/routes.js
+++ client/routes.js
@@ -4,7 +4,7 @@ import {withRouter, Route, Switch} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {Login, Signup, UserHome} from './components';
 import {me} from './store';
-import HomePage from './components/home';
+import HomePage from './components/homepage';
 
 /**
  * COMPONENT
