f219859817a62b62013de6e3e390129dcacec316
Rami Dihu <rami.dihu@gmail.com>
2019-01-08 07:39:39 -0600

    Associations and seed data

---
+++ seedData.js
@@ -0,0 +1,14 @@
+const user = [{"id":1,"first_name":"Gabriel","last_name":"Loughman","email":"gloughman0@tripod.com","gender":"Male"},
+{"id":2,"first_name":"Vilma","last_name":"Shufflebotham","email":"vshufflebotham1@pen.io","gender":"Female"},
+{"id":3,"first_name":"Boy","last_name":"Sindle","email":"bsindle2@ovh.net","gender":"Male"},
+{"id":4,"first_name":"Aura","last_name":"Frede","email":"afrede3@shop-pro.jp","gender":"Female"},
+{"id":5,"first_name":"Amble","last_name":"Wogan","email":"awogan4@stanford.edu","gender":"Male"},
+{"id":6,"first_name":"Carleen","last_name":"Thornley","email":"cthornley5@china.com.cn","gender":"Female"},
+{"id":7,"first_name":"Dede","last_name":"Compston","email":"dcompston6@jigsy.com","gender":"Female"},
+{"id":8,"first_name":"Ripley","last_name":"Preddy","email":"rpreddy7@shop-pro.jp","gender":"Male"},
+{"id":9,"first_name":"Maribel","last_name":"Mapp","email":"mmapp8@businessinsider.com","gender":"Female"},
+{"id":10,"first_name":"Jerrome","last_name":"Guerra","email":"jguerra9@narod.ru","gender":"Male"}];
+
+module.exports = {
+  user
+};
