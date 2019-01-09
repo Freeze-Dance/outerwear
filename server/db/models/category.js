25d34a8a9c6e2276bcc4d93c91829a04e369a8ff
Finn Terdal <fterdal@gmail.com>
2019-01-09 11:00:55 -0600

    comments

--- server/db/models/category.js
+++ server/db/models/category.js
@@ -2,9 +2,9 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Category = db.define('category', {
-  color: {
-    type: Sequelize.STRING,
-    validate: {isIn: [['red', 'blue', 'green', 'black', 'yellow']]}
+  name: {
+    type: Sequelize.STRING
+    // validate: {isIn: [['red', 'blue', 'green', 'black', 'yellow']]}
   }
 })
 
