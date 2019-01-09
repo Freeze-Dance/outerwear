50387f0fc2a20abd7b6910cde515e14a9f780d59
Jay Jongik Thom <ljik95@hotmail.com>
2019-01-09 17:01:29 -0600

    redux store for customer orders complete

--- client/store/order.js
+++ client/store/order.js
@@ -1,1 +1,25 @@
 import axios from 'axios'
+
+const initialState = {customerOrders: [], allOrders: []}
+
+const GET_CUSTOMER_ORDERS = 'GET_CUSTOMER_ORDERS'
+
+const getCustomerOrders = orders => ({type: GET_CUSTOMER_ORDERS, orders})
+
+export const fetchCustomerOrders = userId => async dispatch => {
+  try {
+    const res = await axios.get(`/api/orders/${userId}`)
+    dispatch(getCustomerOrders(res.data))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_CUSTOMER_ORDERS:
+      return {...state, customerOrders: [...action.orders]}
+    default:
+      return state
+  }
+}
