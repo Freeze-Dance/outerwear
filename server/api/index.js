7f0ea9c0dd84d22f3eefbeabebece99306166702
Yaodi Hu <yaodijr@gmail.com>
2019-01-09 18:18:10 -0600

    working on session cart api route

--- server/api/index.js
+++ server/api/index.js
@@ -4,6 +4,12 @@ module.exports = router
 router.use('/users', require('./users'))
 router.use('/products', require('./products'))
 
+router.put('/guestAdd', (req, res, next) => {
+  console.log(req.body)
+  req.session.cart = []
+  res.send('hello')
+})
+
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
