8aeb7eabe8ffeaed4e149cf50f0e066a9c06a71e
Jay Jongik Thom <ljik95@hotmail.com>
2019-01-14 15:07:03 -0600

    oauthh

---
+++ secrets.js
@@ -0,0 +1,6 @@
+process.env.GOOGLE_CLIENT_ID =
+  '729011565876-efj1p7qou7bsg3ea3bhee1cj4ulo31fo.apps.googleusercontent.com'
+
+process.env.GOOGLE_CLIENT_SECRET = 'Z3i3H_SdmPjeWASNfiwaQgN1'
+
+process.env.GOOGLE_CALLBACK = '/auth/google/callback'
