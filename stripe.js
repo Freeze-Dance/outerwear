3465acce9430b71e96897992204ab3a5763c99eb
Yaodi Hu <yaodijr@gmail.com>
2019-01-15 10:11:14 -0600

    commit before checkout

--- stripe.js
+++ stripe.js
@@ -1,10 +1,10 @@
-// Set your secret key: remember to change this to your live secret key in production
-// See your keys here: https://dashboard.stripe.com/account/apikeys
-var stripe = require('stripe')('sk_test_X6BZjn0LR6PzVaTWFtjiTf6k')
+// // Set your secret key: remember to change this to your live secret key in production
+// // See your keys here: https://dashboard.stripe.com/account/apikeys
+// var stripe = require('stripe')('sk_test_X6BZjn0LR6PzVaTWFtjiTf6k')
 
-const charge = stripe.charges.create({
-  amount: 999,
-  currency: 'usd',
-  source: 'tok_visa',
-  receipt_email: 'yaodijr@gmail.com'
-})
+// const charge = stripe.charges.create({
+//   amount: 999,
+//   currency: 'usd',
+//   source: 'tok_visa',
+//   receipt_email: 'yaodijr@gmail.com'
+// })
