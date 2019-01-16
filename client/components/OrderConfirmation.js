8d6698e501cb99ae4fe53758acd0683e715e09ea
Yaodi Hu <yaodijr@gmail.com>
2019-01-16 09:44:33 -0600

    lol

---
+++ client/components/OrderConfirmation.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Typography} from '@material-ui/core'
+
+class OrderConfirmation extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <br />
+        <Typography variant="h2">Order Submitted!</Typography>
+      </React.Fragment>
+    )
+  }
+}
+export default OrderConfirmation
